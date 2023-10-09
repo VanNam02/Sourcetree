460ec89fffd4a3bd835e5f82200c8e1d076070fd
Nam <nam@gamil.com>
2023-10-09 21:01:51 +0700

    led 7 thanh đơn

--- 89c51/main.c
+++ 89c51/main.c
@@ -1,80 +1,16 @@
 #include <REGX51.H>
-#define nutnhan P3_1 
  
+unsigned char port[] = {0x40,0x79,0x24,0x30,0x19,0x12,0x02,0x78,0x00,0x10};
+
 void delay(int value){
-	int x,y ;
-	for(x = 0; x < value; x++){
-		for(y = 0; y < 125; y++){
-		};
-	}
+	int x, y;
+	for(x = 0; x < value; x++)
+		for(y = 0; y < 125; y++);
 }
 void main(){
-	int dem ;
-  nutnhan = 1; 
-	dem = 0;
-	while(1){
-		P2 = 0x00;
-			if(nutnhan == 0){
-				dem ++ ;				
-			}
-			if(dem == 1){	
-				P2_0 = 1;
-				delay(500);
-			}
-			else if(dem == 2){
-				P2_0 = 1;
-				P2_1 = 1;
-				delay(500);
-			}
-			else if(dem == 3){
-				P2_0 = 1;
-				P2_1 = 1;
-				P2_2 = 1;
-				delay(500);
-			}else if(dem == 4){
-				P2_0 = 1;
-				P2_1 = 1;
-				P2_2 = 1;
-				P2_3 = 1;
-				delay(500);
-			}else if(dem == 5){
-				P2_0 = 1;
-				P2_1 = 1;
-				P2_2 = 1;
-				P2_3 = 1;
-				P2_4 = 1;
-				delay(500);
-			}else if(dem == 6){
-				P2_0 = 1;
-				P2_1 = 1;
-				P2_2 = 1;
-				P2_3 = 1;
-				P2_4 = 1;
-				P2_5 = 1;
-				delay(500);
-			}else if(dem == 7){
-				P2_0 = 1;
-				P2_1 = 1;
-				P2_2 = 1;
-				P2_3 = 1;
-				P2_4 = 1;
-				P2_5 = 1;
-				P2_6 = 1;
-				delay(500);
-			}else if(dem == 8){
-				P2_0 = 1;
-				P2_1 = 1;
-				P2_2 = 1;
-				P2_3 = 1;
-				P2_4 = 1;
-				P2_5 = 1;
-				P2_6 = 1;
-				P2_7 = 1;
-				delay(500);
-			}else {
-				P2 = 0x00;
-				delay(500);
-				dem = 0;
-			}
+	int i ;
+	for(i = 0; i < 10; i++){
+		P2 = port[i];
+		delay(1000);
 	}
 }
